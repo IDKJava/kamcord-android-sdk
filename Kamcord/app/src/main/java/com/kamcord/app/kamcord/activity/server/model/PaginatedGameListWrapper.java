17b51655b1da7b35ebebd1447679e4547dca509d
Pat Plunkett <plunkett@kamcord.com>
2015-05-11 16:13:23 -0700

    Whoops, didn't need that.

--- Kamcord/app/src/main/java/com/kamcord/app/kamcord/activity/server/model/PaginatedGameListWrapper.java
+++
@@ -1,9 +0,0 @@
-package com.kamcord.app.kamcord.activity.server.model;
-
-/**
- * Created by pplunkett on 5/11/15.
- */
-public class PaginatedGameListWrapper {
-    public StatusCode status;
-    public PaginatedGameList response;
-}
