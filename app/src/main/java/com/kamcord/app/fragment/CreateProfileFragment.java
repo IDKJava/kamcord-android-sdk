a9200bfbfb8adda7c7e7d5e5e0d74638a4f70c95
Pat Plunkett <plunkett@kamcord.com>
2015-05-13 15:31:14 -0700

    Handles the create profile callback correctly.

--- app/src/main/java/com/kamcord/app/fragment/CreateProfileFragment.java
+++ app/src/main/java/com/kamcord/app/fragment/CreateProfileFragment.java
@@ -1,5 +1,6 @@
 package com.kamcord.app.fragment;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.SpannableStringBuilder;
@@ -11,9 +12,12 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 import com.kamcord.app.R;
+import com.kamcord.app.activity.RecordActivity;
 import com.kamcord.app.server.client.AppServerClient;
 import com.kamcord.app.server.model.Account;
 import com.kamcord.app.server.model.GenericResponse;
+import com.kamcord.app.server.model.StatusCode;
+import com.kamcord.app.utils.AccountManager;
 import com.kamcord.app.utils.StringUtils;
 
 import butterknife.ButterKnife;
@@ -71,16 +75,33 @@ public class CreateProfileFragment extends Fragment {
         AppServerClient.getInstance().createProfile(username, email, password, createProfileCallback);
     }
 
+    private void handleLoginFailure(GenericResponse<Account> accountWrapper)
+    {
+        AccountManager.clearStoredAccount();
+        // TODO: show the user something about failing to create a profile here.
+    }
+
     private Callback<GenericResponse<Account>> createProfileCallback = new Callback<GenericResponse<Account>>()
     {
         @Override
-        public void success(GenericResponse<Account> accountGenericResponse, Response response) {
-
+        public void success(GenericResponse<Account> accountWrapper, Response response) {
+            if( accountWrapper != null
+                    && accountWrapper.status != null && accountWrapper.status.equals(StatusCode.OK)
+                    && accountWrapper.response != null )
+            {
+                AccountManager.setStoredAccount(accountWrapper.response);
+                Intent intent = new Intent(getActivity(), RecordActivity.class);
+                startActivity(intent);
+            }
+            else
+            {
+                handleLoginFailure(accountWrapper);
+            }
         }
 
         @Override
         public void failure(RetrofitError error) {
-
+            handleLoginFailure(null);
         }
     };
 }
