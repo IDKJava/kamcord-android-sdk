43145484aacad9f07be1194204e3625f6d4a00f0
Don <donliang1@Dons-MacBook-Pro.local>
2015-05-12 22:40:21 -0700

    add: webview for fragment

--- app/src/main/java/com/kamcord/app/fragment/WatchFragment.java
+++ app/src/main/java/com/kamcord/app/fragment/WatchFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.kamcord.app.R;
 
@@ -13,9 +15,15 @@ import com.kamcord.app.R;
  */
 public class WatchFragment extends Fragment{
 
+    private String kamcordURL = "https://www.kamcord.com/games/";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.profile_tab, container, false);
+        WebView wv = (WebView) v.findViewById(R.id.webView);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.setWebViewClient(new WebViewClient());
+        wv.loadUrl(kamcordURL);
         return v;
     }
 }
