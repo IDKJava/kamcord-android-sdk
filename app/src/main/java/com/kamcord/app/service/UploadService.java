23d32587c2ae5aee9d8a0bad6f08d1d5ee0cfd08
Pat Plunkett <plunkett@kamcord.com>
2015-05-16 16:20:41 -0700

    Reserves a video.

--- app/src/main/java/com/kamcord/app/service/UploadService.java
+++ app/src/main/java/com/kamcord/app/service/UploadService.java
@@ -4,7 +4,12 @@ import android.app.IntentService;
 import android.content.Intent;
 
 import com.kamcord.app.model.RecordingSession;
+import com.kamcord.app.server.client.AppServerClient;
+import com.kamcord.app.server.model.ReserveVideoEntity;
 import com.kamcord.app.server.model.ReserveVideoResponse;
+import com.kamcord.app.server.model.builder.ReserveVideoEntityBuilder;
+
+import retrofit.RetrofitError;
 
 public class UploadService extends IntentService {
     private static final String TAG = RecordingService.class.getSimpleName();
@@ -19,6 +24,23 @@ public class UploadService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         RecordingSession recordingSession = intent.getParcelableExtra(ARG_SESSION_TO_SHARE);
+
+        ReserveVideoEntity reserveVideoEntity = new ReserveVideoEntityBuilder()
+                .setUserTitle(recordingSession.getVideoTitle())
+                .setDescription(recordingSession.getVideoDescription())
+                .setDefaultTitle("default title") // TODO: fill this in with something that makes sense.
+                .setGameId(recordingSession.getGameServerID())
+                .build();
+
+        ReserveVideoResponse reserveVideoResponse;
+        try
+        {
+            reserveVideoResponse = AppServerClient.getInstance().reserveVideo(reserveVideoEntity);
+        }
+        catch( RetrofitError e )
+        {
+            e.printStackTrace();
+        }
     }
 
     public interface UploadCallback
