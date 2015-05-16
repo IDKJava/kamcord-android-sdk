38cfae63495a185d41c9db1da8aaf3866c57611c
Pat Plunkett <plunkett@kamcord.com>
2015-05-16 15:46:43 -0700

    Let's keep this out for another PR.

--- app/src/main/java/com/kamcord/app/service/UploadService.java
+++ app/src/main/java/com/kamcord/app/service/UploadService.java
@@ -2,16 +2,9 @@ package com.kamcord.app.service;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.util.Log;
 
-import com.google.gson.Gson;
 import com.kamcord.app.model.RecordingSession;
-import com.kamcord.app.server.client.AppServerClient;
-import com.kamcord.app.server.model.ReserveVideoEntity;
 import com.kamcord.app.server.model.ReserveVideoResponse;
-import com.kamcord.app.server.model.builder.ReserveVideoEntityBuilder;
-
-import retrofit.RetrofitError;
 
 public class UploadService extends IntentService {
     private static final String TAG = RecordingService.class.getSimpleName();
@@ -26,25 +19,6 @@ public class UploadService extends IntentService {
     @Override
     protected void onHandleIntent(Intent intent) {
         RecordingSession recordingSession = intent.getParcelableExtra(ARG_SESSION_TO_SHARE);
-
-        ReserveVideoEntity reserveVideoEntity = new ReserveVideoEntityBuilder()
-                .setUserTitle(recordingSession.getVideoTitle())
-                .setDescription(recordingSession.getVideoDescription())
-                .setDefaultTitle("default title") // TODO: fill this in with something that makes sense.
-                .setGameId(recordingSession.getGameServerID())
-                .build();
-
-        ReserveVideoResponse reserveVideoResponse;
-        try
-        {
-            Log.v("FindMe", "Attempting to reserve video...");
-            reserveVideoResponse = AppServerClient.getInstance().reserveVideo(reserveVideoEntity);
-            Log.v("FindMe", "Received response: " + new Gson().toJson(reserveVideoResponse));
-        }
-        catch( RetrofitError e )
-        {
-            e.printStackTrace();
-        }
     }
 
     public interface UploadCallback
