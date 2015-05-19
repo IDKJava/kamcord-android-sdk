6b907123b096fde0a6b9d161f4e29c686a613ede
Don <donliang1@Dons-MacBook-Pro.local>
2015-05-19 13:19:59 -0700

    fix mediacontroller not ready issue

--- app/src/main/java/com/kamcord/app/activity/VideoPreviewActivity.java
+++ app/src/main/java/com/kamcord/app/activity/VideoPreviewActivity.java
@@ -5,7 +5,6 @@ import android.content.pm.ActivityInfo;
 import android.media.MediaMetadataRetriever;
 import android.media.MediaPlayer;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.MediaController;
@@ -37,15 +36,16 @@ public class VideoPreviewActivity extends Activity {
         mVideoView = (VideoView) findViewById(R.id.videoview_preview);
         replayImageBtn = (ImageButton) findViewById(R.id.replayButton);
         final String videoPath = getIntent().getExtras().getString(ARG_VIDEO_PATH);
-        Log.d("VideoPath", videoPath);
 
         // Determine videoview orientation
         mediaMetadataRetriever = new MediaMetadataRetriever();
-        mediaMetadataRetriever.setDataSource(videoPath);
-        videoHeight = Integer.parseInt(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT));
-        videoWidth = Integer.parseInt(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH));
-        if(videoHeight <= videoWidth) {
-            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        if(videoPath != null) {
+            mediaMetadataRetriever.setDataSource(videoPath);
+            videoHeight = Integer.parseInt(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_HEIGHT));
+            videoWidth = Integer.parseInt(mediaMetadataRetriever.extractMetadata(MediaMetadataRetriever.METADATA_KEY_VIDEO_WIDTH));
+            if(videoHeight <= videoWidth) {
+                setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+            }
         }
 
         replayImageBtn.setOnClickListener(new View.OnClickListener() {
@@ -57,12 +57,12 @@ public class VideoPreviewActivity extends Activity {
         });
 
         if(mediaController == null) {
-            mediaController = new MediaController(getApplicationContext());
-        }
-        try {
-            mVideoView.setMediaController(mediaController);
-        } catch (Exception e) {
-            e.printStackTrace();
+            mediaController = new MediaController(this);
+            try {
+                mVideoView.setMediaController(mediaController);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
 
         mVideoView.setVideoPath(videoPath);
