ccf268e345c85492d88f4ae045b492dc787faf35
Don <donliang1@Dons-MacBook-Pro.local>
2015-05-26 10:56:06 -0700

    minor changes

--- app/src/main/java/com/kamcord/app/view/DynamicRecyclerView.java
+++ app/src/main/java/com/kamcord/app/view/DynamicRecyclerView.java
@@ -1,9 +1,12 @@
-package com.kamcord.app.utils;
+package com.kamcord.app.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.AttributeSet;
+
+import com.kamcord.app.R;
 
 /**
  * Created by donliang1 on 5/22/15.
@@ -11,7 +14,8 @@ import android.util.AttributeSet;
 public class DynamicRecyclerView extends RecyclerView {
 
     private GridLayoutManager gridLayoutManager;
-    private int columnWidth = 300;
+    private int columnWidth = 0;
+    private int columnNumber = 3;
 
     public DynamicRecyclerView(Context context) {
         super(context);
@@ -29,15 +33,20 @@ public class DynamicRecyclerView extends RecyclerView {
     }
 
     private void init(Context context, AttributeSet attrs) {
-        gridLayoutManager = new GridLayoutManager(getContext(), 3);
+        if (attrs != null) {
+            TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.AutoGridItem);
+            columnWidth = array.getInteger(R.styleable.AutoGridItem_GridItemWidth, 300);
+            array.recycle();
+        }
+        gridLayoutManager = new GridLayoutManager(getContext(), columnNumber);
         setLayoutManager(gridLayoutManager);
     }
 
     @Override
     protected void onMeasure(int widthSpec, int heightSpec) {
         super.onMeasure(widthSpec, heightSpec);
-            int spanCount = Math.max(3, getMeasuredWidth() / columnWidth);
-            gridLayoutManager.setSpanCount(spanCount);
+        int spanCount = Math.max(columnNumber, getMeasuredWidth() / columnWidth);
+        gridLayoutManager.setSpanCount(spanCount);
     }
 
 }
