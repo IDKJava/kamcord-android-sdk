f747c9c7667ac9fae1fa51df98e67cd5f95b8809
Don <donliang1@Dons-MacBook-Pro.local>
2015-05-29 14:55:48 -0700

    fix: videoview bug

--- app/src/main/java/com/kamcord/app/activity/ProfileVideoViewActivity.java
+++ app/src/main/java/com/kamcord/app/activity/ProfileVideoViewActivity.java
@@ -1,5 +1,7 @@
 package com.kamcord.app.activity;
 
+import android.content.pm.ActivityInfo;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -11,7 +13,7 @@ import com.kamcord.app.R;
 import butterknife.ButterKnife;
 import butterknife.InjectView;
 
-public class ProfileVideoViewActivity extends AppCompatActivity {
+public class ProfileVideoViewActivity extends AppCompatActivity implements MediaPlayer.OnPreparedListener {
 
     public static final String M3U8_VIDEO_PATH = "profilevideo";
 
@@ -38,9 +40,25 @@ public class ProfileVideoViewActivity extends AppCompatActivity {
 
         if(url != null) {
             myVideoView.setVideoURI(Uri.parse(url));
+            myVideoView.setOnPreparedListener(this);
             myVideoView.start();
             myVideoView.requestFocus();
         }
+    }
 
+    @Override
+    public void onPrepared(MediaPlayer mp) {
+        int videoWidth = mp.getVideoWidth();
+        int videoHeight = mp.getVideoHeight();
+        if (videoHeight <= videoWidth) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        } else {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
     }
 }
