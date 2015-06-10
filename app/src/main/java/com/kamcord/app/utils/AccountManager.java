a138cabecb0777c426f5a13aae38e7d7ee958c2e
dqueue <dbyqin@gmail.com>
2015-06-09 18:16:30 -0700

    added an account listener and fix profile refresh bug

--- app/src/main/java/com/kamcord/app/utils/AccountManager.java
+++ app/src/main/java/com/kamcord/app/utils/AccountManager.java
@@ -7,18 +7,44 @@ import android.util.Log;
 import com.google.gson.Gson;
 import com.kamcord.app.server.model.Account;
 
+import java.util.ArrayList;
+
 /**
  * Created by pplunkett on 5/13/15.
  */
 public class AccountManager {
     private static final String ACCOUNT_PREFS = "ACCOUNT_PREFS";
     private static final String ACCOUNT = "ACCOUNT";
+    private static final Object accountLock = new Object();
     private static SharedPreferences preferences = null;
+    private static boolean currentLoginState = false;
+    private static ArrayList<AccountListener> accountListeners = new ArrayList<AccountListener>();
 
     public synchronized static void initializeWith(Context context)
     {
         preferences = context.getApplicationContext()
                 .getSharedPreferences(ACCOUNT_PREFS, Context.MODE_PRIVATE);
+        currentLoginState = isLoggedIn();
+    }
+
+    public static void addListener(AccountListener listener)
+    {
+        synchronized(accountLock)
+        {
+            if( accountListeners != null ) {
+                accountListeners.add(listener);
+            }
+        }
+    }
+
+    public static void removeListener(AccountListener listener)
+    {
+        synchronized(accountLock)
+        {
+            if( accountListeners != null ) {
+                accountListeners.remove(listener);
+            }
+        }
     }
 
     public static boolean isLoggedIn()
@@ -26,6 +52,7 @@ public class AccountManager {
         Account account = getStoredAccount();
         return account != null && account.token != null;
     }
+
     public static Account getStoredAccount()
     {
         Account account = null;
@@ -44,11 +71,28 @@ public class AccountManager {
         return account;
     }
 
+    private static void notifyLoginStateChanged(boolean state)
+    {
+        synchronized (accountLock)
+        {
+            if( accountListeners != null )
+            {
+                for( AccountListener listener : accountListeners )
+                    listener.isLoggedInChanged(state);
+            }
+        }
+    }
+
     public static void setStoredAccount(Account account)
     {
         if( preferences != null )
         {
             preferences.edit().putString(ACCOUNT, new Gson().toJson(account)).apply();
+            if( isLoggedIn() != currentLoginState )
+            {
+                currentLoginState = isLoggedIn();
+                notifyLoginStateChanged(currentLoginState);
+            }
         }
     }
 
@@ -57,6 +101,11 @@ public class AccountManager {
         if( preferences != null )
         {
             preferences.edit().remove(ACCOUNT).apply();
+            if( isLoggedIn() != currentLoginState )
+            {
+                currentLoginState = isLoggedIn();
+                notifyLoginStateChanged(currentLoginState);
+            }
         }
     }
 }
