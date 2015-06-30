c7b816e7e355d48d7223b7814dbcf586f321cfd2
Mehmet Gerceker <mehmetg@msn.com>
2015-06-29 22:08:35 -0700

    Doxygen jenkins fix.

--- app/src/androidTest/builder/generateDoxygen.sh
+++ app/src/androidTest/builder/generateDoxygen.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-DOXYGEN_BIN=$(which doxygen)
-if [ -z "$DOXYGEN_BIN" ]; then
+DOXYGEN_BIN="/usr/local/bin/doxygen"
+if [ -x $DOXYGEN_BIN ]; then
     "Please install doxygen on your system before running this script! e.g. brew install doxygen"
     exit 1
 fi
@@ -8,7 +8,7 @@ DOCDIR="app/build/testDocs/html"
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 rm -rf $DOCDIR
 mkdir -p $DOCDIR
-doxygen $DIR/doxygenConfig.cfg
+$DOXYGEN_BIN $DIR/doxygenConfig.cfg
 echo "******"
 echo "Documents generated can be found under $DOCDIR/annotated.html"
 echo "******"
