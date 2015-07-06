3344c1ff76bb9b2db06c67f26bc37a055a2e0a56
Pat Plunkett <plunkett@kamcord.com>
2015-07-06 11:27:18 -0700

    Hides the keyboard on outside touches in the login fragment.

--- app/src/main/java/com/kamcord/app/utils/KeyboardUtils.java
+++ app/src/main/java/com/kamcord/app/utils/KeyboardUtils.java
@@ -15,4 +15,20 @@ public class KeyboardUtils {
             imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
         }
     }
+
+    public static void showSoftKeyboard(View view, Context context) {
+        InputMethodManager imm = (InputMethodManager) context.getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.showSoftInput(view, 0);
+//        if(imm.isActive() == true && view.getWindowToken() != null) {
+//            imm.showSoftInputFromInputMethod(view.getWindowToken(), 0);
+//        }
+    }
+
+    public static void setSoftKeyboardVisibility(View view, Context context, boolean visible) {
+        if( visible ) {
+            showSoftKeyboard(view, context);
+        } else {
+            hideSoftKeyboard(view, context);
+        }
+    }
 }
