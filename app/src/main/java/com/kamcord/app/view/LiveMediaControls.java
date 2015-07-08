fca76b7b75bf5389eb9b249d734b91d8c1881fd9
Pat Plunkett <plunkett@kamcord.com>
2015-07-07 20:09:28 -0700

    Trying to figure out stupid seekbars.

--- app/src/main/java/com/kamcord/app/view/LiveMediaControls.java
+++ app/src/main/java/com/kamcord/app/view/LiveMediaControls.java
@@ -3,6 +3,7 @@ package com.kamcord.app.view;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.PorterDuff;
+import android.graphics.drawable.Drawable;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,6 +13,7 @@ import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.MediaController;
 import android.widget.RelativeLayout;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 import com.kamcord.app.R;
@@ -46,6 +48,8 @@ public class LiveMediaControls implements MediaControls {
     TextView usernameTextView;
     @InjectView(R.id.follow_button)
     Button followButton;
+    @InjectView(R.id.scrubber_seekbar)
+    SeekBar scrubberSeekBar;
 
     public LiveMediaControls(Context context, Video video) {
         root = (RelativeLayout) LayoutInflater.from(context).inflate(R.layout.view_live_media_controls, null);
@@ -60,6 +64,9 @@ public class LiveMediaControls implements MediaControls {
             ((ViewGroup) anchorView).addView(root);
 
             ButterKnife.inject(this, root);
+            Drawable thumb = root.getResources().getDrawable(R.drawable.solid_white_circle_12dp);
+            thumb.setColorFilter(android.R.color.white, PorterDuff.Mode.SRC_IN);
+            scrubberSeekBar.setThumb(thumb);
 
             if( video != null && video.user != null ) {
                 User owner = video.user;
