ae80e3901a4bcdd96c1c1509392632e7d05208c4
dqueue <dennis@kamcord.com>
2015-07-13 16:44:55 -0700

    Update VideoViewActivity.java

--- app/src/main/java/com/kamcord/app/activity/VideoViewActivity.java
+++ app/src/main/java/com/kamcord/app/activity/VideoViewActivity.java
@@ -250,7 +250,6 @@ public class VideoViewActivity extends AppCompatActivity implements
 
     @Override
     public void onStateChanged(boolean playWhenReady, int playbackState) {
-        System.out.println("QQQQ " + playbackState);
         if (playbackState == Player.STATE_READY) {
             mediaControls.show(playWhenReady ? 3000 : 0, true);
         }
